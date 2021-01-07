2a3989777807579a15b808c96bde6f54dd9779b2
AlexeyMerkulov <alexeymerkulovv@gmail.com>
2021-01-07 18:01:21 +0300

    0. Map, HashMap [#367859]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("petrov@gmail.com", "Petrov Ivan Sergeevich");
+        map.put("vasilev@yandex.ru", "Vasilev Petr Borisovich");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
