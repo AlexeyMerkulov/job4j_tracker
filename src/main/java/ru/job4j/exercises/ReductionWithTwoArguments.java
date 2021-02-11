486bc830c25c2477688a26546da7dffea0369667
AlexeyMerkulov <alexeymerkulovv@gmail.com>
2021-02-11 21:33:15 +0300

    0.4. Понятие редукции. Метод reduce(). Редукция с начальным значением

---
+++ src/main/java/ru/job4j/exercises/ReductionWithTwoArguments.java
@@ -0,0 +1,18 @@
+/**
+ * Иногда нам может понадобиться произвести редукцию с начальным значение.
+ * Для этого есть перегруженный метод reduce().
+ * Первый аргумент начальное значение, второй оператор.
+ * В отличие от первого метода, который принимает один аргумент,
+ * этот метод возвращает начальное значение для пустого списка, а не Optional.
+ * Ваша задача умножить 5 на элементы списка
+ */
+package ru.job4j.exercises;
+
+import java.util.List;
+
+public class ReductionWithTwoArguments {
+    public static Integer collect(List<Integer> list) {
+        return list.stream()
+                .reduce(5, (n1, n2) -> n1 * n2);
+    }
+}
