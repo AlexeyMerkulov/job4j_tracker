e70717091bcf42bfc10a3622456e6a13143ef741
AlexeyMerkulov <alexeymerkulovv@gmail.com>
2021-02-14 11:37:32 +0300

    8. Удаление одного списка из другого

---
+++ src/main/java/ru/job4j/exercises/SplitterList.java
@@ -0,0 +1,24 @@
+/**
+ * Задание: метод принимает 3 списка элементов:
+ *
+ * 1. Общий список элементов;
+ *
+ * 2. Список важных элементов, но не обязательных;
+ *
+ * 3. Список элементов, которые обязательно должны отсутствовать в результирующем списке.
+ *
+ * Необходимо реализовать метод, который вернет список, основой которого будет первый,
+ * присутствовать элементы из второго и точно отсутствовать элементы из третьего.
+ */
+
+package ru.job4j.exercises;
+
+import java.util.List;
+
+public class SplitterList {
+    public static List<String> split(List<String> left, List<String> middle, List<String> right) {
+        left.retainAll(middle);
+        left.removeAll(right);
+        return left;
+    }
+}
