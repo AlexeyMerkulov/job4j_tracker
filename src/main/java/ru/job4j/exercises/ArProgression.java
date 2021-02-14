ca2e7745cbe65bf1d67ed1639ace672056aa8927
AlexeyMerkulov <alexeymerkulovv@gmail.com>
2021-02-14 18:07:18 +0300

    14. Арифметическая прогрессия(upd)

--- src/main/java/ru/job4j/exercises/ArProgression.java
+++ src/main/java/ru/job4j/exercises/ArProgression.java
@@ -7,9 +7,7 @@ public class ArProgression {
         int sum = 0;
         boolean logic = true;
         for (int i = 1; i < data.size() - 1; i++) {
-            if ((data.get(i - 1) + data.get(i + 1)) * 0.5 == data.get(i)) {
-                logic = true;
-            } else {
+            if ((data.get(i - 1) + data.get(i + 1)) * 0.5 != data.get(i)) {
                 logic = false;
                 break;
             }
