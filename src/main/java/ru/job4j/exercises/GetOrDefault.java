6ac0f45d3c48bbdb87d3eb8c63ff3e4e243d528a
AlexeyMerkulov <alexeymerkulovv@gmail.com>
2021-02-16 21:26:26 +0300

    4. Возврат отличных от null значений по умолчанию.

---
+++ src/main/java/ru/job4j/exercises/GetOrDefault.java
@@ -0,0 +1,25 @@
+/**
+ * Задание: необходимо реализовать метод,
+ * который позволит добавить элемент list(значение) с ключом index
+ * в хранилище store только в том случае, если такого ключа пока еще нет в хранилище.
+ * Метод getValue() должен вернуть значение которое соответствует ключу index,
+ * если такого ключа нет - необходимо вернуть пустой список.
+ */
+
+package ru.job4j.exercises;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GetOrDefault {
+    private Map<Integer, List<String>> store = new HashMap<>();
+
+    public void addValue(int index, List<String> list) {
+        store.putIfAbsent(index, list);
+    }
+
+    public List<String> getValue(int index) {
+        return store.getOrDefault(index, List.of());
+    }
+}
