871b1a2f7b338d047c105c838f69705a91d0f031
AlexeyMerkulov <alexeymerkulovv@gmail.com>
2021-02-26 22:30:26 +0300

    1. Генератор текста(исправление)

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -1,15 +1,23 @@
 package ru.job4j.collection;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
+import java.util.Set;
 
 public class Article {
     public static boolean generateBy(String origin, String line) {
-        List<String> originList = Arrays.asList(origin.split("[.,!:;? ]"));
-        HashSet<String> hashSet = new HashSet<>(originList);
-        List<String> lineList = Arrays.asList(line.split("[.,!:;? ]"));
-        return !hashSet.addAll(lineList);
+        boolean rsl = true;
+        String[] originArray = origin.split("[.,!:;? ]");
+        String[] lineArray = line.split("[.,!:;? ]");
+        Set<String> hashSet = new HashSet<>();
+        for (String value : originArray) {
+            hashSet.add(value);
+        }
+        for (String value : lineArray) {
+            if (!hashSet.contains(value)) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
     }
 }
