57217d72ab02a68545733efef9b93cb04147d0b5
AlexeyMerkulov <alexeymerkulovv@gmail.com>
2021-02-26 23:25:27 +0300

    2. Проверить две строчки на идентичность.(исправление)

--- src/main/java/ru/job4j/collection/FreezeStr.java
+++ src/main/java/ru/job4j/collection/FreezeStr.java
@@ -1,35 +1,32 @@
 package ru.job4j.collection;
 
-        /*
-        List<String> leftList = Arrays.asList(left.split(""));
-        List<String> rightList = Arrays.asList(right.split(""));
-        Collections.sort(leftList);
-        Collections.sort(rightList);
-        return leftList.equals(rightList);
-        */
-
 import java.util.*;
 
 public class FreezeStr {
     public static boolean eq(String left, String right) {
-        String[] leftArray = left.split("");
-        String[] rightArray = right.split("");
-        Map<String, Integer> mapLeft = new HashMap<>();
-        Map<String, Integer> mapRight = new HashMap<>();
-        for (String s1 : leftArray) {
-            if (mapLeft.containsKey(s1)) {
-                mapLeft.compute(s1, (key, value) -> value + 1);
+        boolean rsl = true;
+        char[] leftArray = left.toCharArray();
+        char[] rightArray = right.toCharArray();
+        Map<Character, Integer> check = new HashMap<>();
+        for (Character c : leftArray) {
+            if (check.containsKey(c)) {
+                check.compute(c, (key, value) -> value + 1);
             } else {
-                mapLeft.put(s1, 1);
+                check.put(c, 1);
             }
         }
-        for (String s2 : rightArray) {
-            if (mapRight.containsKey(s2)) {
-                mapRight.compute(s2, (key, value) -> value + 1);
-            } else {
-                mapRight.put(s2, 1);
+        for (Character c : rightArray) {
+            if (!check.containsKey(c)) {
+                rsl = false;
+                break;
             }
+            if (check.containsKey(c) && check.get(c) == 1) {
+                check.remove(c);
+                continue;
+            }
+            check.compute(c, (key, value) -> value - 1);
         }
-        return mapLeft.equals(mapRight);
+        return rsl;
     }
 }
+
